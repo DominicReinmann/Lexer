16505e063ddb55f92a7931166aba8b3a34927f3a
DominicReinmann <reinmann.dominic@bluewin.ch>
2023-12-19 13:42:56 +0100

    inti commit

---
+++ Lexer.cs
@@ -0,0 +1,4 @@
+namespace Lexer
+{
+   public static class Lexer 
+}
---
+++ Token.cs
@@ -0,0 +1,21 @@
+namespace Lexer
+{
+   public enum Tokens {
+	   Ident, 
+	   Integer,
+	   Let,
+	   Illegal,
+	   Eof,
+	   Equals, 
+	   Plus,
+	   Comma,
+	   Semicolon,
+	   LParen,
+	   RParen, 
+	   LSquirly, 
+	   RSQuirly,
+	   Function,
+	   String,
+	   Identifier
+   } 
+}
